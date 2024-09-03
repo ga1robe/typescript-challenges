cdbf62608b5096de244e42ac4d8381e49a8f1d91
Robert Gajewski <robert.gajewski@gmail.com>
2024-09-03 18:49:03 +0200

    changes: <T extends {id:number;name:string}> in processItems function; change bug TS2322: return type of the key from T[] to Pick<T,id|name>[]; this is a type that is a new type from T, only and name are affected

--- src/constraints/constraints.ts
+++ src/constraints/constraints.ts
@@ -44,7 +44,9 @@ const videos: Video[] = [
   { id: 5, name: 'Poznaj TypeScript', length: 22 },
 ];
 
-function processItems<T>(items: T[]): T[] {
+function processItems<T extends { id: number; name: string }>(
+  items: T[]
+): Pick<T, 'id' | 'name'>[] {
   return items.filter(({ id }) => id > 2).map(({ id, name }) => ({ id, name }));
 }
 
